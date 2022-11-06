50707737648a07912bb162a7e07d0ec0359be1d4
Benny <benny-wagner@web.de>
2022-11-06 19:03:23 +0100

    imports cleaned

--- src/components/ConvexHull/ConvexHull.tsx
+++ src/components/ConvexHull/ConvexHull.tsx
@@ -1,7 +1,5 @@
-import Konva from "konva";
 import React, { useState } from "react";
-import { Stage, Layer, Rect, Text, Circle, Line } from "react-konva";
-import { nanoid } from "nanoid";
+import { Stage, Layer, Circle, Line } from "react-konva";
 import { Point, Vector } from "../../lib/geometry";
 import { grahamScan } from "../../lib/algorithms/graham";
 
@@ -50,8 +48,8 @@ function ConvexHull() {
   return (
     <Stage
       className="bg-slate-800 rounded-xl"
-      width={Math.max(800, window.innerWidth * 0.8)}
-      height={Math.max(600, window.innerHeight * 0.8)}
+      width={Math.min(800, window.innerWidth * 0.8)}
+      height={Math.min(600, window.innerHeight * 0.8)}
       onClick={handleClick}
     >
       <Layer>
