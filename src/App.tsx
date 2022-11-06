ac824d5dc505e902998a1cd0e44f78e4816acfdf
Tobias Schmücker <schmuecker.tobias@gmail.com>
2022-11-06 20:39:35 +0100

    Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ConvexHull from "./components/ConvexHull/ConvexHull";
 
 function App() {
   return (
-    <div className="p-16">
+    <div className="p-4 sm:p-16">
       <h1 className="text-xl">Convex Hull</h1>
       <ConvexHull />
     </div>
