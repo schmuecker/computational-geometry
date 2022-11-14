bf7106d75157580eee027de940c28115e8d96b1b
Tobias Schmücker <schmuecker.tobias@gmail.com>
2022-11-14 14:51:15 +0100

    Update SweepLine.tsx

--- src/components/SweepLine/SweepLine.tsx
+++ src/components/SweepLine/SweepLine.tsx
@@ -11,11 +11,7 @@ function SweepLine() {
     setSegments((state) => [...state, newSegment]);
   };
 
-  const seg1 = new Vector(new Point(10, 2), new Point(0, 2));
-  const seg2 = new Vector(new Point(2, 0), new Point(2, 5));
-  const seg3 = new Vector(new Point(6, 3), new Point(6, 6));
-  const seg4 = new Vector(new Point(1, 4), new Point(8, 4));
-  const result = isoSweep([seg1, seg2, seg3, seg4]);
+  const result = isoSweep(segments);
   console.log(result);
   return (
     <div>
