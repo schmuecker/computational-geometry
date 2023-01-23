3883a022fc39a76741904ef850662bbb8039e3cb
Benny <benny-wagner@web.de>
2023-01-23 08:13:12 +0100

    twoD fix

--- src/lib/algorithms/two-d-tree/twoDTree.ts
+++ src/lib/algorithms/two-d-tree/twoDTree.ts
@@ -218,7 +218,7 @@ class TwoDTree {
     if (!this.rootNode) {
       return;
     }
-    rangeSearch(this.rootNode, "ver", queryRange);
+    rangeSearch(this.rootNode, "hor", queryRange);
     return { output, visited };
   }
 }
