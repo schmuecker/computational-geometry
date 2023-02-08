a7ad11583adeab511cf6cd366fb08c8f64be1c0d
Benny <benny-wagner@web.de>
2023-02-08 15:02:09 +0100

    damping fix

--- src/lib/algorithms/newtons-method/newtonsMethod.ts
+++ src/lib/algorithms/newtons-method/newtonsMethod.ts
@@ -20,14 +20,41 @@ function newtonsMethod({
   let x = startX;
 
   for (let i = 0; i < maxIter; i++) {
-    const y = fn(x);
-    const m = dfn(x);
-    const c = y - m * x;
+    // intuitive approach
+    // xi+1 = x + si
+    // si = -f(x) / df(x)
 
-    // 0 = mx + c
-    // -c = mx
-    // -c/m = x
-    const approxRoot = -c / m;
+    // const y = fn(x);
+    // const m = dfn(x);
+    // const c = y - m * x;
+
+    // // 0 = mx + c
+    // // -c = mx
+    // // -c/m = x
+    // const approxRoot = -c / m;
+
+    const si = -(fn(x) / dfn(x));
+
+    // Damping
+    // lambda = max(1, 1/2, 1/3, 1/4, …. lambda_min) so, dass
+    let lambda = 1;
+
+    if (damping) {
+      const lambda_list = [1, 1 / 2, 1 / 4, 1 / 8, 1 / 16];
+
+      for (let index = 0; index < lambda_list.length; index++) {
+        lambda = lambda_list[index];
+
+        // check the condition ( break for the first lambda that fulfills condition to get the max possible lambda)
+        // f(xi)² > f(xi + lambda * si)²
+        if (Math.pow(fn(x), 2) > Math.pow(fn(x + lambda * si), 2)) {
+          break;
+        }
+      }
+    }
+
+    const approxRoot = x + lambda * si;
+
     approxRootList.push(approxRoot);
 
     // quit loop if accuracy is achieved
